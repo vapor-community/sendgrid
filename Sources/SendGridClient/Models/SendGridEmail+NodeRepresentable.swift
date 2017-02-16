960fb0cc4c3acb5b5bfad8201d36af06f260ddfe
Toby Griffin <toby.griffin@me.com>
2017-02-16 11:41:36 +1100

    Progress

--- Sources/SendGridClient/Models/SendGridEmail+NodeRepresentable.swift
+++ Sources/SendGridClient/Models/SendGridEmail+NodeRepresentable.swift
@@ -21,9 +21,11 @@ extension SendGridEmail: NodeRepresentable {
             obj["content"] = Node(content.map {
                 switch $0.type {
                 case .html:
-                    return Node(["type": "text/html", "value": $0.content.makeNode()])
+                    return Node(["type": "text/html",
+                                 "value": $0.content.makeNode()])
                 case .plain:
-                    return Node(["type": "text/plain", "value": $0.content.makeNode()])
+                    return Node(["type": "text/plain",
+                                 "value": $0.content.makeNode()])
                 }
             })
         }
@@ -69,37 +71,86 @@ extension SendGridEmail: NodeRepresentable {
         if let ipPoolName = ipPoolName {
             obj["ip_pool_name"] = Node(ipPoolName)
         }
-        // Mail settings
-        obj["mail_settings"] = try {
-            var ms = Node([:])
-            // BCC
-            if let bccFirst = bccFirst {
-                ms["bcc", "enable"] = true
-                ms["bcc", "email"] = try bccFirst.makeNode()
-            }
-            // Bypass List Management
-            if bypassListManagement {
-                ms["bypass_list_management", "enable"] = true
-            }
-            // Footer
-            if !footer.isEmpty {
-                ms["footer", "enable"] = true
-                footer.forEach {
-                    switch $0.type {
-                    case .html:
-                        ms["footer", "html"] = Node($0.content)
-                    case .plain:
-                        ms["footer", "plain"] = Node($0.content)
-                    }
+        /// MAIL SETTINGS
+        var ms = Node([:])
+        // BCC
+        if let bccFirst = bccFirst {
+            ms["bcc", "enable"] = true
+            ms["bcc", "email"] = try bccFirst.makeNode()
+        }
+        // Bypass List Management
+        if bypassListManagement {
+            ms["bypass_list_management", "enable"] = true
+        }
+        // Footer
+        if !footer.isEmpty {
+            ms["footer", "enable"] = true
+            footer.forEach {
+                switch $0.type {
+                case .html:
+                    ms["footer", "html"] = Node($0.content)
+                case .plain:
+                    ms["footer", "text"] = Node($0.content)
                 }
             }
-            // Sandbox Mode
-            if sandboxMode || true {
-                ms["sandbox_mode", "enable"] = true
-            }
-            return ms
-        }()
-
+        }
+        // Sandbox Mode
+        if sandboxMode {
+            ms["sandbox_mode", "enable"] = true
+        }
+        // Spam Check
+        switch spamCheckMode {
+        case let .enabled(threshold, url):
+            ms["spam_check", "enable"] = true
+            ms["spam_check", "threshold"] = Node(threshold)
+            ms["spam_check", "post_to_url"] = Node(url)
+        case .disabled:
+            break
+        }
+        obj["mail_settings"] = ms
+        /// TRACKING SETTINGS
+        var ts = Node([:])
+        // Click Tracking
+        switch clickTracking {
+        case .enabled:
+            ts["click_tracking", "enable"] = true
+            ts["click_tracking", "enable_text"] = true
+        case .htmlOnly:
+            ts["click_tracking", "enable"] = true
+            ts["click_tracking", "enable_text"] = false
+        case .disabled:
+            break
+        }
+        // Open Tracking
+        switch openTracking {
+        case .enabled(let substitutionTag):
+            ts["open_tracking", "enable"] = true
+            ts["open_tracking", "substitution_tag"] = substitutionTag?.makeNode()
+        case .disabled:
+            break
+        }
+        // Subscription Tracking
+        switch subscriptionManagement {
+        case let .appending(text, html):
+            ts["subscription_tracking", "enable"] = true
+            ts["subscription_tracking", "text"] = text?.makeNode()
+            ts["subscription_tracking", "html"] = html?.makeNode()
+        case .enabled(let replacingTag):
+            ts["subscription_tracking", "enable"] = true
+            ts["subscription_tracking", "substitution_tag"] = Node(replacingTag)
+        case .disabled:
+            break
+        }
+        // Google Analytics
+        if let ga = googleAnalytics {
+            ts["ganalytics", "enable"] = true
+            ts["ganalytics", "utm_source"] = ga.source?.makeNode()
+            ts["ganalytics", "utm_medium"] = ga.medium?.makeNode()
+            ts["ganalytics", "utm_term"] = ga.term?.makeNode()
+            ts["ganalytics", "utm_content"] = ga.content?.makeNode()
+            ts["ganalytics", "utm_campaign"] = ga.campaign?.makeNode()
+        }
+        obj["tracking_settings"] = ts
         return obj
     }
 
