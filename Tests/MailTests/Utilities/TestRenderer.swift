35d0cf37c6714701996aab55aad75605ba6a013d
Toby Griffin <toby.griffin@gmail.com>
2017-04-07 11:46:45 +1000

    Tidy comments

--- Tests/MailTests/Utilities/TestRenderer.swift
+++ Tests/MailTests/Utilities/TestRenderer.swift
@@ -1,31 +1,5 @@
 import Vapor
 
-// final class TestRenderer: ViewRenderer {
-//     let viewsDir: String
-//     var views: [String: Bytes]
-//
-//     init(viewsDir: String) {
-//         self.viewsDir = viewsDir
-//         self.views = [:]
-//     }
-//
-//     enum Error: Swift.Error {
-//         case viewNotFound
-//     }
-//
-//     func make(_ path: String, _ context: Node) throws -> Vapor.View {
-//       return try make(path, context, for: nil)
-//     }
-//
-//     func make(_ path: String, _ context: Node, for provider: Provider.Type?) throws -> View {
-//         guard let bytes = self.views[path] else {
-//             throw Error.viewNotFound
-//         }
-//
-//         return View(data: bytes)
-//     }
-// }
-
 final class TestRenderer: ViewRenderer {
 
     init(viewsDir: String) {}
