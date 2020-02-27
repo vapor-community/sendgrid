24a3cc2591b33dfe2554e12502db89d331d496ae
Ralph Küpper <ralph.kuepper@skelpo.com>
2020-02-27 10:48:36 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SendGrid",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         .library(name: "SendGrid", targets: ["SendGrid"])
