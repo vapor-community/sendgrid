faf483481677542a678d1bb1b379b2cd32a224dd
Ralph Küpper <ralph.kuepper@skelpo.com>
2020-03-25 21:13:18 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,21 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "SendGrid",
     platforms: [
-       .macOS(.v10_15)
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "SendGrid", targets: ["SendGrid"])
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
-        ],
+    ],
     targets: [
-        .target(name: "SendGrid", dependencies: ["Vapor"]),
-        .testTarget(name: "SendGridTests", dependencies: ["Vapor", "SendGrid"])
+        .target(name: "SendGrid", dependencies: [
+            .product(name: "Vapor", package: "vapor")
+        ]),
+        .testTarget(name: "SendGridTests", dependencies: ["SendGrid"])
     ]
 )
