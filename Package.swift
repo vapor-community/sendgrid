5e1ab343c3a2a4f733232d5a6f569728efa1c093
Claudio Gomez <cagvcr26@gmail.com>
2020-07-28 13:02:44 -0600

    Updated sendgrid-kit to support optional  property

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor-community/sendgrid-kit.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor-community/sendgrid-kit.git", from: "1.0.1"),
     ],
     targets: [
         .target(name: "SendGrid", dependencies: [
