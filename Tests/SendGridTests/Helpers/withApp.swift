6bed9e08f6d93a4ea3f1a1f2818deabb20d541e3
Francesco Paolo Severino <96546612+fpseverino@users.noreply.github.com>
2024-10-06 12:54:06 +0200

    Update Tests/SendGridTests/Helpers/withApp.swift
    Co-authored-by: Mahdi Bahrami <github@mahdibm.com>

--- Tests/SendGridTests/Helpers/withApp.swift
+++ Tests/SendGridTests/Helpers/withApp.swift
@@ -3,7 +3,7 @@ import Vapor
 
 func withApp(_ body: (Application) async throws -> Void) async throws {
     let app = try await Application.make(.testing)
-    try #require(isLoggingConfigured == true)
+    try #require(isLoggingConfigured)
     try await body(app)
     try await app.asyncShutdown()
 }
