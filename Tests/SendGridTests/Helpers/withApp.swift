98a8e6e715b0a887c13c0e6a20a75db097b5e284
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-01-29 18:50:00 +0100

    Update to VaporTesting

--- Tests/SendGridTests/Helpers/withApp.swift
+++
@@ -1,9 +0,0 @@
-import Testing
-import Vapor
-
-func withApp(_ body: (Application) async throws -> Void) async throws {
-    let app = try await Application.make(.testing)
-    try #require(isLoggingConfigured)
-    try await body(app)
-    try await app.asyncShutdown()
-}
